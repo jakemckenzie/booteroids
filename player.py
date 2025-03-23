ca06e26c45966f71eede380c165be6657cb18174
Jake McKenzie <jake314@uw.edu>
2025-03-22 22:03:24 -0600

    added rate limitting to the shot mechanic

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     pygame.sprite.Sprite.__init__(self, *Player.containers)
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.shoot_timer = 0.0
 
 
   def rotate(self, dt): 
@@ -32,10 +33,14 @@ class Player(CircleShape):
     pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
   def shoot(self):
-    shot = Shot(self.position.x, self.position.y)
-    shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    if self.shoot_timer <= 0:
+      shot = Shot(self.position.x, self.position.y)
+      shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+      self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
   def update(self, dt):
+    self.shoot_timer = max(self.shoot_timer - dt, 0)
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]: self.move(dt)
